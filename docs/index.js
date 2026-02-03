e992d77af9a2b85c3ed75a24db9105c6754b7dcb
Pablo Lusarreta <pablo.lusarreta@gmail.com>
2026-02-03 14:27:32 +0100

    03022025.14.27

--- docs/index.js
+++ docs/index.js
@@ -1,99 +1,54 @@
-const idiomas = ['castellano', 'euskera']
-let idioma_sel, foto, DATOS
-const inicio = () => {
-    // CONSTANTES
-    const cabecera = document.getElementsByTagName('header')[0]
-    const secciones = document.getElementsByTagName('section')[0]
-    const pie = document.getElementsByTagName('footer')[0]
-    const titular = document.getElementById('titular')
-    const eslogan = document.getElementById('eslogan')
-    const coletilla = document.getElementById('coletilla')
-    const imgPortada = document.getElementById('imgPortada')
-    imgPortada.addEventListener('load', () => {
-        imgPortada.style.opacity = '1'
-    })
-    // ARRANQUE
-    fetch("data.json")
-        .then(response => response.json())
-        .then(data => {
-            DATOS = data
-            cabecera.innerHTML = `<div>${fechaActual()}</div>
-                                    <div> 
-                                        <a href="https://api.whatsapp.com/send?phone=34678194512&text=" target="_blank"><img src="img/whatsapp.png">watsapp</a> 
-                                        <a href="tel:34678194512" target="_blank"><img src="img/telefono.png">llamar</a> 
-                                        <a href="mailto:iwonamasaje@gmail.com" target="_blank"><img src="img/email.png">e-mail</a> 
-                                    </div>`
-            titular.innerHTML = `<div></div>
-                                <div>${data.titular.titulo}</div>
-                                <div>${data.titular.descripcion}</div>`
-            eslogan_pres()
-            presentacion()
-
-            coletilla.innerHTML = data.titular.coletilla
-
-            secciones.innerHTML = ''
-            for (const i in data.secciones) {
-                secciones.innerHTML += `<div style="background-image:url(img/${i}.jpg)">
-                                            <div>${data.secciones[i].precio} €</div>
-                                            <div>${data.secciones[i].titulo} 
-                                                <div><img src="./img/relog.png">${data.secciones[i].duracion}</div>
-                                            </div>
-                                            <div>${data.secciones[i].texto}
-                                            </div> 
-                                        </div>`
-            }
-
-            for (const i in data.pie) {
-                pie.innerHTML += `<div><img src="img/${data.pie[i].img}">
-                                <span>${data.pie[i].titulo}</span>
-                                <br>
-                                ${data.pie[i].texto}</div>`
-            }
-            fotosInicio()
-            movil()
+const cargaContenido = url => {
+    fetch(url)
+        .then(res => res.text())
+        .then(html => {
+            document.getElementById('cuerpo').innerHTML = html;
         })
+        .catch(err => console.log(err));
 }
-const presentacion = () => {
-    imgPortada.style.opacity = '0'
-    setTimeout(() => {
-        imgPortada.src = `img/inicio/${DATOS.imagenes[foto]}`
-        foto = (foto == (DATOS.imagenes.length - 1)) ? 0 : foto + 1
-    }, 500)
-}
-const eslogan_pres = () => {
-    let num = Math.floor(DATOS.titular.eslogan.length * Math.random())
-    eslogan.innerHTML = DATOS.titular.eslogan[num]
-    eslogan.style.opacity = '1'
-}
-const fechaActual = () => {
-    const hoy = new Date()
-    const diasSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"]
-    const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
-    return (`${diasSemana[hoy.getDay()]}, ${hoy.getDate()} de ${meses[hoy.getMonth()]} de ${hoy.getFullYear()}`)
-}
+// MOVILES
 const movil = () => {
     if (navigator.userAgent.match(/Android/i)
-        || navigator.userAgent.match(/webOS/i)
-        || navigator.userAgent.match(/iPhone/i)
-        || navigator.userAgent.match(/iPad/i)
-        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/iPhone/i)        
         || navigator.userAgent.match(/BlackBerry/i)
         || navigator.userAgent.match(/Windows Phone/i)) {
-        document.getElementById("estilo").setAttribute('href', 'movil.css')
+
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = 'movil.css';
+        const head = document.head;
+        head.insertBefore(link, head.firstChild);
     }
 }
-const fotosInicio = () => {
-    fetch('fotosInicio', {
-        method: 'POST',
-        body: '',
-        headers: {
-            'Content-Type': 'application/json'
+
+
+window.onload = () => {
+    // PRESENTACION  
+    const imgHome = ["H4.jpg", "H2.jpg", "H3.jpg", "H1.jpg"]
+    const imgPresentacion = document.getElementById("presentacion")
+    const imgPres = document.getElementById("imgPres")
+    let foto = 0
+    let alter = true
+    imgPres.addEventListener("load", () => {
+        imgPres.style.opacity = '1'
+    })
+    const presentacion = () => {
+        if (alter) {
+            imgPres.src = `img/${imgHome[foto]}`
+            setTimeout(() => {
+                imgPresentacion.style.backgroundImage = `url('img/${imgHome[foto]}')`;
+            }, 3000)
+            alter = false
+        } else {
+            imgPres.style.opacity = '0'
+            alter = true
         }
-    }).then(res => res.json())
-        .then(ficheros => {
-            DATOS.imagenes = ficheros
-            foto = Math.floor(DATOS.imagenes.length * Math.random())
-            setInterval(presentacion, 5000)
-        })
-}
-//window.onload = inicio
+        foto = (foto == (imgHome.length - 1)) ? 0 : foto + 1
+    }
+    //ARRANQUE
+    setInterval(presentacion, 10000)
+    movil()
+
+
+
+} 
